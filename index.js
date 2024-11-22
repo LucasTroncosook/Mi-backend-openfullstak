095983828de9701faf7344aa25ddea15e1357b40
Lucas <LucasTroncoso176@gmail.com>
2024-11-22 18:52:28 -0300

    mongodb

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+require('dotenv').config()
 const express = require("express");
 const cors = require("cors");
+const Note = require('./models/note')
+
 const app = express();
 
 app.use(cors());
@@ -43,7 +46,9 @@ app.get('/', (request,response) => {
 });
 
 app.get('/api/notes', (request,response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+      response.json(notes)
+    })
 });
 
 app.get('/api/notes/:id', (request, response) => {
@@ -94,7 +99,7 @@ app.post('/api/notes', (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
